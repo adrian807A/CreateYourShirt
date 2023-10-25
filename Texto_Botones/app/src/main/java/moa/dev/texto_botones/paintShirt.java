08c60c2d8a34af90d52b0b51c2cb989fd9cdd5da
adrian807A <141088624+adrian807A@users.noreply.github.com>
2023-10-25 17:26:30 -0300

    app Botones agregado

---
+++ Texto_Botones/app/src/main/java/moa/dev/texto_botones/paintShirt.java
@@ -0,0 +1,80 @@
+package moa.dev.texto_botones;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class paintShirt extends AppCompatActivity {
+    Button btnOrder, btnph;
+    ImageView imageView1, imageView;
+    //ImageView imageView1;
+    TextView  textSize, textDat;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_paint_shirt);
+        //imgView = findViewById(R.id.imgView1);
+        imageView1 = findViewById(R.id.imageView1);
+        imageView = findViewById(R.id.imgView1);
+        //textView = findViewById(R.id.textView);
+        textSize = findViewById(R.id.textSize);
+        textDat = findViewById(R.id.textDat);
+        btnph = findViewById(R.id.btnph);
+
+        Bundle bundle = getIntent().getExtras();
+        if (bundle!= null)
+        {
+            int res_image = bundle.getInt("my_image");
+            imageView1.setImageResource(res_image);
+
+            String dato = getIntent().getStringExtra("dato");
+            textSize.setText(dato);//("size" + dato)
+
+            String data = getIntent().getStringExtra("data");
+            textDat.setText(data);//("size" + dato)
+        }
+
+
+        Bundle b = getIntent().getBundleExtra("bundle");
+        //encadeno imagen
+
+        //fin
+        byte[] bytearry = b.getByteArray("photo");
+
+        Bitmap bmp = BitmapFactory.decodeByteArray(bytearry, 0,bytearry.length);
+        imageView.setImageBitmap(bmp);
+
+
+
+        // intent.putExtra("my_image", drawable.whitwho);
+
+
+
+
+
+        // Bundle extras = getIntent().getExtras();
+        // int res_image = bundle.getInt("my_image");
+        // imageView1.setImageResource(res_image);
+        // String bitmap=extras.getString("ImagePath");
+        // String ImageName= extras.getString("ImageName");
+
+
+
+    }
+    //Metodo botón anterior
+    public void Anterior(View view){
+        Intent anterior = new Intent( this, MainActivity.class);
+        startActivity(anterior);
+
+    }
+
+}
